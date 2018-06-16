a6441f09918fb871b709b68c45ab6197ec5c2fab
Winter <XunMengWinter@gmail.com>
2018-06-16 15:31:13 +0800

    export realm file

--- extract_realm_database_android.sh
+++ extract_realm_database_android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ADB_PATH="/Users/ice/Library/Android/sdk/platform-tools"
-PACKAGE_NAME="top.wefor.now"
-DB_NAME="now.realm"
+PACKAGE_NAME="top.wefor.nowview.test"
+DB_NAME="now_test.realm"
 DESTINATION_PATH="/Users/ice/Downloads/${DB_NAME}"
 NOT_PRESENT="List of devices attached"
 ADB_FOUND=`${ADB_PATH}/adb devices | tail -2 | head -1 | cut -f 1 | sed 's/ *$//g'`
